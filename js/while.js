6d03bcfc377626a37d49504cc09857777eb2602d
tysonwmccrary <tysonwmccrary@gmail.com>
2017-04-17 12:49:22 -0400

    new changes all new updates

--- js/while.js
+++ js/while.js
@@ -37,20 +37,57 @@
 //
 // outputTarget.innerHTML = outputHtml;
 <!--Exercise 5-->
-var n = 15;
-var outputTarget = document.querySelector("#exercise5");
-var outputHtml = "<ul>";
+// var n = 15;
+// var outputTarget = document.querySelector("#exercise5");
+// var outputHtml = "<ul>";
+//
+// while (n > 0) {
+//     console.log(n);
+//     if ( n % 2 === 0) {
+//         outputHtml += "<li>even</li>";
+//     } else {
+//         outputHtml += "<li>odd</li>";
+//     }
+//     n--;
+// }
+//
+// outputHtml = "</ul>";
+//
+// outputTarget.innerHTML = outputHtml;
 
-while (n > 0) {
-    console.log(n);
-    if ( n % 2 === 0) {
-        outputHtml += "<li>even</li>";
-    } else {
-        outputHtml += "<li>odd</li>";
-    }
-    n--;
+<!--Rock, Paper, Sissors-->
+
+￼var userChoice = prompt("Choose Rock, Paper, or Scissors by typing 'R', 'P', or 'S'");
+
+var rng = Math.random();
+ var computerChoice = "R";
+ if(rng > 0.66) {
+     computerChoice = "P";
+ } else if (rng > 0.33) {
+     computerChoice = "S";
 }
 
-outputHtml = "</ul>";
-
-outputTarget.innerHTML = outputHtml;
+var userWins = "You win!";
+ var computerWins = "The computer wins!";
+ if(computerChoice !== userChoice){
+     if(computerChoice === "R"){
+         if (userChoice === "S") {
+             alert(computerWins);
+         } else {
+             alert(userWins);
+         }
+     } else if (computerChoice === "P") {
+         if( userChoice === "R") {
+             alert(computerWins);
+         } else {
+             alert(userWins);
+}
+} else {
+         if( userChoice === "P") {
+             alert(computerWins);
+         } else {
+             alert(userWins);
+} 
+ } else {
+     alert('Tie!');
+}
